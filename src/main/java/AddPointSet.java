9eb1e989ded8538b0a63ca911e78a8b9184c566d
Seiji Shaw <seiji.shaw@gmail.com>
2017-08-23 16:51:35 -0700

    Added file parsing functionality

--- src/main/java/AddPointSet.java
+++ src/main/java/AddPointSet.java
@@ -1,7 +1,60 @@
+import ij.plugin.Commands;
+import net.imagej.ImageJ;
+
+import org.scijava.Context;
+import org.scijava.ItemIO;
+import org.scijava.command.Command;
+import org.scijava.log.LogService;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+import java.io.File;
+
+@Plugin(type = Command.class)
+public class AddPointSet implements Command {
+
+    //To report errors (if necessary)
+    @Parameter
+    private LogService logService;
+
+    //To obtain text file (must be output from Nikon software) TODO: get software name
+    @Parameter
+    private File pointSet;
 
 
-public class AddPointSet {
+    @Override
+    public void run() {
+
+        //FOR TESTING PURPOSES ONLY - IN PRODUCTION, PAN WILL BE RECEIVED FROM CONTEXT
+        Pan pan = new Pan();
+
+        //ensure we have a text file
+        String name = pointSet.getName();
+        if (name.toLowerCase().lastIndexOf(".txt")!= name.length()-4) {
+            logService.error(new IllegalArgumentException("Please specify a file with .txt extension"));
+            return;
+        }
+
+        //attempt to load dataset into plugin memory
+        try {
+            pan.loadFile(pointSet);
+        } catch (Exception err) {
+            logService.error(err);
+        }
+
+    }
 
 
+    //STRICTLY for testing purposes
+    public static void main(final String... args) {
+        // Launch ImageJ as usual.
+        final ImageJ ij = net.imagej.Main.launch(args);
 
+
+        // Launch command right away
+        ij.command().run(AddPointSet.class, true);
+    }
 }
+
+
+
---
+++ src/main/java/Pan.java
@@ -0,0 +1,97 @@
+import org.scijava.plugin.Plugin;
+import org.scijava.service.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+
+/*
+Plugin service for all PointsANalysis operation. User interface is given through command plugins.
+
+TODO: Allow for operation on multiple point datasets from different textfiles (introduce linking?)
+ */
+@Plugin(type = Service.class)
+public class Pan {
+
+    //Ensure we are reading the correct type of text file by checking the first line (should always be the same)
+    private static final String CHECK_STRING = "Channel Name\tX\tY\tXc\tYc\tHeight\tArea\tWidth\tPhi\tAx\tBG\tI\tFrame\tLength\tLink\tValid\tZ\tZc\tPhotons\tLateral Localization Accuracy\tXw\tYw\tXwc\tYwc\tZw\tZwc";
+
+    //Channels available
+    private ArrayList<Channel> channels;
+
+    public Pan() {
+        channels = new ArrayList<>();
+    }
+
+    //Find the channel by name in channels list
+    private int findChannel(String channelName) {
+        for (int i = 0; i < channels.size(); i++) {
+            if (channels.get(i).getName().equals(channelName)) return i;
+        }
+
+        return -1;
+    }
+
+    //Data is delimited by tab characters. This 'function' (initiator function and recursive body) can skip to the
+    //nth tab of a provided string
+    private int skipTabs(String line, int n) {
+        return findNextTab(line, n, 0);
+    }
+    private int findNextTab(String line, int n, int currentPos) {
+        if (n == 0) return currentPos-1;
+        return findNextTab(line,n-1, line.indexOf('\t', currentPos)+1);
+    }
+
+
+    //Parse file and load into channels list
+    public void loadFile(File file) throws IllegalArgumentException, IOException {
+
+        //read file data line by line into list(thank you Java 8!)
+        ArrayList<String> rawInput = (ArrayList <String>) Files.readAllLines(file.toPath());
+
+        //Ensure we have the right filetype
+        if (rawInput == null || !rawInput.get(0).equals(CHECK_STRING)) {
+            throw new IllegalArgumentException("Incorrect file content. Please ensure file was exported properly");
+        }
+
+        //some initialization so we don't need to constantly discard references
+        String line;
+        String channelName;
+        int x;
+        int y;
+        int z;
+
+        //parse the rest of the file and load points into channels list
+        for (int i = 1; i < rawInput.size(); i++) {
+
+            //grab line, take channel name
+            line = rawInput.get(i);
+            channelName = line.substring(0, line.indexOf('\t'));
+
+            //check if we have a channel named 'channelName' already in 'channels', if not, create
+            if (findChannel(channelName) < 0) {
+                channels.add(new Channel(channelName));
+            }
+
+            //Find x,y,z value based on tab delimitation
+            //Note: this may work for now, but these hardcoded values may need to be more flexible
+            x = (int)(Double.parseDouble(line.substring(skipTabs(line, 4)+1, skipTabs(line, 5)))+0.5);
+            y = (int)(Double.parseDouble(line.substring(skipTabs(line, 5)+1, skipTabs(line, 6)))+0.5);
+            z = (int)(Double.parseDouble(line.substring(skipTabs(line, 13)+1, skipTabs(line, 14)))+0.5);
+
+            //add proper point to proper channel
+            channels.get(findChannel(channelName)).getPoints().add(new Point(x,y,z));
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+}
