1359e33156c4fe387589b67d9f66564a06dfa27c
Seiji Shaw <seiji.shaw@gmail.com>
2017-09-14 16:15:18 -0700

    Added path finding for export command

--- src/main/java/cmds/ExportAnalysis.java
+++ src/main/java/cmds/ExportAnalysis.java
@@ -1,11 +1,12 @@
 package cmds;
 
 import cmds.gui.ChannelModuleItem;
+import ij.gui.GenericDialog;
+import ij.io.SaveDialog;
 import org.scijava.command.Command;
-import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
-import java.nio.file.Path;
+import java.io.File;
 import java.util.List;
 
 /**
@@ -14,14 +15,41 @@ import java.util.List;
 @Plugin(type = Command.class, menuPath = "PAN>Export analysis...")
 public class ExportAnalysis extends OutputAnalysisCommand {
 
-    @Parameter
-    private Path path;
 
     @Override
     public void run() {
         List <ChannelModuleItem <Boolean>> checkedItems = getCheckedModules();
 
+        //Get file directory to use, if user hits cancel, getFile will return null, and the command will
+        //exit
+        File export = getFile("");
+        if (export == null) return;
 
+
+    }
+
+    private String getPath(String defaultName) {
+        SaveDialog sd = new SaveDialog("Export Analysis", defaultName, ".txt");
+        String path = sd.getDirectory() + sd.getFileName();
+        if (path.equals("nullnull")) return null;
+        return path;
+    }
+
+    private File getFile(String defaultName) {
+        String path = getPath(defaultName);
+        if (path == null) return null;
+        File export = new File(path);
+        if (export.exists()) {
+            GenericDialog gd = new GenericDialog("Confirm Save");
+            gd.addMessage("'" + export.getName() + "' already exists. Would you like to replace it?");
+            gd.setOKLabel("Replace");
+            gd.showDialog();
+            if (gd.wasCanceled()) {
+                return getFile(defaultName);
+            }
+        }
+
+        return export;
     }
 
 
