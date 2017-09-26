0054dea3745e5c7fbbbe6a054790e97d0610a93e
Seiji Shaw <seiji.shaw@gmail.com>
2017-09-26 14:56:13 -0700

    Implemented cross-channel nearest neighbor analysis algorithm export

--- src/main/java/cmds/analysis/ExportCrossNearestNeighborText.java
+++ src/main/java/cmds/analysis/ExportCrossNearestNeighborText.java
@@ -1,10 +1,9 @@
 package cmds.analysis;
 
-import analysis.data.Linear;
+import analysis.data.ListPointContainer;
 import analysis.data.OperablePointContainer;
-import analysis.data.PointContainer;
-import analysis.ops.LinearNearestNeighbor;
-import cmds.DynamicOutputSingleChannel;
+import analysis.ops.CrossLinearNearestNeighbor;
+import cmds.DynamicOutputDoubleChannel;
 import ij.io.SaveDialog;
 import org.scijava.command.Command;
 import org.scijava.plugin.Plugin;
@@ -13,37 +12,31 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Map;
-import java.util.Set;
 
-/**
- * Exports file with analysis data (double list)
- */
-@Plugin(type = Command.class, menuPath = "PAN>Export analysis...")
-public class ExportAnalysisText extends DynamicOutputSingleChannel {
+@Plugin(type = Command.class, menuPath = "PAN>Export analysis...>")
+public class ExportCrossNearestNeighborText extends DynamicOutputDoubleChannel {
 
 
-    @Override
     public void run() {
         //Get file directory to use, if user hits cancel, getFile will return null, and the command will
         //exit
         File export = getFile("");
         if (export == null) return;
 
-        String result = "Channel\tValue\r\n";
+        String result = "FromChannel->ToChannel\tValue\r\n";
 
-        Map <String, PointContainer> checkedChannels = getCheckedChannels();
-        Set <String> channelNames = checkedChannels.keySet();
+        OperablePointContainer from = getFromChannel();
+        OperablePointContainer to = getToChannel();
 
-        OperablePointContainer channel;
-        double[] nearestNeighborResult;
+        String fromName = getFromName();
+        String toName = getToName();
 
-        for (String name : channelNames) {
-            nearestNeighborResult = new LinearNearestNeighbor((Linear) checkedChannels.get(name)).process();
-            for (double value : nearestNeighborResult) {
-                result += name + "\t" + value + "\r\n";
-            }
+        double[] nearestNeighborResult = new CrossLinearNearestNeighbor((ListPointContainer) from, (ListPointContainer) to).execute();
+
+        for (double value : nearestNeighborResult) {
+            result += fromName + "->" + toName + "\t" + value + "\r\n";
         }
+
 
         try (BufferedWriter writer = Files.newBufferedWriter(export.toPath())) {
             writer.write(result);
@@ -54,8 +47,9 @@ public class ExportAnalysisText extends DynamicOutputSingleChannel {
 
     }
 
+
     private String getPath(String defaultName) {
-        SaveDialog sd = new SaveDialog("Export Analysis", defaultName, ".txt");
+        SaveDialog sd = new SaveDialog("Export Nearest Neighbor Analysis", defaultName, ".txt");
         String path = sd.getDirectory() + sd.getFileName();
         if (path.equals("nullnull")) return null;
         return path;
@@ -77,6 +71,4 @@ public class ExportAnalysisText extends DynamicOutputSingleChannel {
 
         return export;
     }
-
-
 }
---
+++ src/main/java/cmds/analysis/ExportNearestNeighborText.java
@@ -0,0 +1,82 @@
+package cmds.analysis;
+
+import analysis.data.Linear;
+import analysis.data.OperablePointContainer;
+import analysis.data.PointContainer;
+import analysis.ops.LinearNearestNeighbor;
+import cmds.DynamicOutputSingleChannel;
+import ij.io.SaveDialog;
+import org.scijava.command.Command;
+import org.scijava.plugin.Plugin;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Exports file with analysis data (double list)
+ */
+@Plugin(type = Command.class, menuPath = "PAN>Export analysis...>Nearest Neighbor")
+public class ExportNearestNeighborText extends DynamicOutputSingleChannel {
+
+
+    @Override
+    public void run() {
+        //Get file directory to use, if user hits cancel, getFile will return null, and the command will
+        //exit
+        File export = getFile("");
+        if (export == null) return;
+
+        String result = "Channel\tValue\r\n";
+
+        Map <String, PointContainer> checkedChannels = getCheckedChannels();
+        Set <String> channelNames = checkedChannels.keySet();
+
+        OperablePointContainer channel;
+        double[] nearestNeighborResult;
+
+        for (String name : channelNames) {
+            nearestNeighborResult = new LinearNearestNeighbor((Linear) checkedChannels.get(name)).process();
+            for (double value : nearestNeighborResult) {
+                result += name + "\t" + value + "\r\n";
+            }
+        }
+
+        try (BufferedWriter writer = Files.newBufferedWriter(export.toPath())) {
+            writer.write(result);
+        } catch (IOException e) {
+
+        }
+
+
+    }
+
+    private String getPath(String defaultName) {
+        SaveDialog sd = new SaveDialog("Export Nearest Neighbor Analysis", defaultName, ".txt");
+        String path = sd.getDirectory() + sd.getFileName();
+        if (path.equals("nullnull")) return null;
+        return path;
+    }
+
+    private File getFile(String defaultName) {
+        String path = getPath(defaultName);
+        if (path == null) return null;
+        File export = new File(path);
+//        if (export.exists()) {
+//            GenericDialog gd = new GenericDialog("Confirm Save");
+//            gd.addMessage("'" + export.getName() + "' already exists. Would you like to replace it?");
+//            gd.setOKLabel("Replace");
+//            gd.showDialog();
+//            if (gd.wasCanceled()) {
+//                return getFile(defaultName);
+//            }
+//        }
+
+        return export;
+    }
+
+
+}
