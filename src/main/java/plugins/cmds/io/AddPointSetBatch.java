ebc2682ad1b45b5bde0ee92aa96561705fa6d5dd
sageshoyu <seiji.shaw@gmail.com>
2019-07-16 21:04:36 -0400

    Implement batch import

--- src/main/java/plugins/cmds/io/AddPointSetBatch.java
+++ src/main/java/plugins/cmds/io/AddPointSetBatch.java
@@ -1,5 +1,6 @@
 package plugins.cmds.io;
 
+import datastructures.points.ChannelContainer;
 import org.scijava.ItemIO;
 import org.scijava.ItemVisibility;
 import org.scijava.command.Command;
@@ -9,8 +10,10 @@ import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.PathMatcher;
+import java.util.ArrayList;
 
 @Plugin(type = Command.class,menuPath = "PAN>Add point set batch from text file")
 public class AddPointSetBatch extends TextImportCommand {
@@ -18,6 +21,7 @@ public class AddPointSetBatch extends TextImportCommand {
 
     @Parameter(label = "Crop images to fit dataset")
     private boolean isRelative;
+
 
     @Parameter(label="Exported .txt file from Nikon Elements", callback = "updateExpression")
     private File dummySet;
@@ -43,12 +47,15 @@ public class AddPointSetBatch extends TextImportCommand {
         }
         searchExpression = name;
     }
-    private void updateFilesFound() {
-        //build pattern
-        int total = 0;
-        String pattern = searchExpression;
-        PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:" + pattern);
 
+    //Note: callback does not trigger on autofill, so user does need to modify directory to
+    //get file found numbers to show
+    private void updateFilesFound() {
+        //build pattern and matcher
+        int total = 0;
+        String pattern = "**" + searchExpression + "*";
+        PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:" + pattern);
+        //loop through all files in current directory to see if they match the pattern
         for (File file : dummySet.getParentFile().listFiles()) {
             if(matcher.matches(file.toPath()))
                 total++;
@@ -59,6 +66,31 @@ public class AddPointSetBatch extends TextImportCommand {
 
     @Override
     public void run() {
+        //Find files that match the search expression, extract ChannelSet, then give to PanContext
+        String pattern = "**" + searchExpression + "*";
+        PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:" + pattern);
+
+        for (File file : dummySet.getParentFile().listFiles()) {
+            if(matcher.matches(file.toPath())) {
+                ChannelContainer newData = null;
+                try {
+                    newData = loadFile(file);
+                } catch (IOException e) {
+                    logService.error(e);
+
+                } finally {
+                    String channelSetName = addPostDuplicateString(file.getName());
+                    if(isRelative) newData.makeRelative();
+                    ptStore.addChannelSet(channelSetName, newData);
+
+                }
+
+
+
+            }
+        }
+
+
 
     }
 
