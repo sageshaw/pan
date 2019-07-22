74862719d000873bd92680e1d32441e829d983bb
sageshoyu <seiji.shaw@gmail.com>
2019-07-22 00:01:05 -0400

    Create factored out histogram and analysis result commands

---
+++ src/main/java/plugins/cmds/DataCommand.java
@@ -0,0 +1,74 @@
+package plugins.cmds;
+
+import datastructures.analysis.DataContainer;
+import ij.gui.GenericDialog;
+import org.apache.commons.math3.exception.NullArgumentException;
+import org.scijava.Initializable;
+import org.scijava.command.DynamicCommand;
+import org.scijava.module.MutableModuleItem;
+import org.scijava.plugin.Parameter;
+import plugins.PanService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class DataCommand extends DynamicCommand implements Initializable {
+
+    @Parameter
+    private PanService panService;
+
+    @Parameter(label = "Analysis result", choices = {"a", "b"})
+    private String dataName;
+
+    @Override
+    public void initialize() {
+        ArrayList<String> options = new ArrayList<>();
+
+        String[] dataKeys = panService.analysisResultKeys();
+
+        if (dataKeys.length == 0) throw new NullArgumentException();
+
+        for (String dataKey : dataKeys)
+            options.add(dataKey);
+
+        MutableModuleItem<String> selectDataItem = getInfo().getMutableInput("dataName", String.class);
+
+        selectDataItem.setChoices(options);
+
+    }
+
+    @Override
+    public void run() {
+
+        DataContainer dataset = panService.getAnalysisResult(dataName);
+
+        setup(dataName, dataset);
+
+        if (dataset.isBatched()) {
+            GenericDialog batchDialogue = new GenericDialog("Found batch...");
+            batchDialogue.addMessage("'" + dataName + "' was found to be in a batch. Run operation on entire batch?");
+            batchDialogue.setOKLabel("Yes");
+            batchDialogue.setCancelLabel("No");
+            batchDialogue.showDialog();
+
+            if (batchDialogue.wasOKed()) {
+                List<DataContainer> datasets = panService.getAnalysisBatch(dataset.getBatchKey());
+                for (DataContainer d : datasets) {
+                    forEveryDataset(panService.analysisResultKey(d), d, true);
+
+                }
+                return;
+            }
+
+        }
+
+        forEveryDataset(dataName, dataset, false);
+    }
+
+
+    protected abstract void setup(String dataName, DataContainer dataset);
+
+    protected abstract void forEveryDataset(String dataName, DataContainer dataset, boolean isBatched);
+
+
+}
---
+++ src/main/java/plugins/cmds/HistogramCommand.java
@@ -0,0 +1,72 @@
+package plugins.cmds;
+
+import datastructures.graphs.HistogramDatasetPlus;
+import ij.gui.GenericDialog;
+import org.apache.commons.math3.exception.NullArgumentException;
+import org.scijava.Initializable;
+import org.scijava.command.DynamicCommand;
+import org.scijava.module.MutableModuleItem;
+import org.scijava.plugin.Parameter;
+import plugins.PanService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class HistogramCommand extends DynamicCommand implements Initializable {
+
+    @Parameter
+    private PanService panService;
+
+    @Parameter(label = "Histogram data", choices = {"a", "b"})
+    private String histoName;
+
+    @Override
+    public void initialize() {
+        ArrayList<String> options = new ArrayList<>();
+
+        String[] histoKeys = panService.histoKeys();
+
+        if (histoKeys.length == 0) throw new NullArgumentException();
+
+        for (String histoKey : histoKeys)
+            options.add(histoKey);
+
+        MutableModuleItem<String> selectHistoItem = getInfo().getMutableInput("histoName", String.class);
+
+        selectHistoItem.setChoices(options);
+    }
+
+    @Override
+    public void run() {
+
+        HistogramDatasetPlus histoData = panService.getHistoSet(histoName);
+
+        setup(histoName, histoData);
+
+        if (histoData.isBatched()) {
+            GenericDialog batchDialogue = new GenericDialog("Found batch...");
+            batchDialogue.addMessage("'" + histoName + "' was found to be in a batch. Run operation on entire batch?");
+            batchDialogue.setOKLabel("Yes");
+            batchDialogue.setCancelLabel("No");
+            batchDialogue.showDialog();
+
+            if (batchDialogue.wasOKed()) {
+                List<HistogramDatasetPlus> histos = panService.getHistoBatch(histoData.getBatchKey());
+                for (HistogramDatasetPlus h : histos) {
+                    forEveryHistoDo(panService.histoKey(h), h, true);
+                }
+                return;
+            }
+        }
+
+        forEveryHistoDo(histoName, histoData, false);
+
+
+    }
+
+    protected abstract void setup(String histoName, HistogramDatasetPlus histoData);
+
+    protected abstract void forEveryHistoDo(String histoName, HistogramDatasetPlus histoData, boolean isBatched);
+
+
+}
