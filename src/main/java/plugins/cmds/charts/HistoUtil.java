9a31f4ea466dc55e875a5be37ca868f64ae176d5
sageshoyu <seiji.shaw@gmail.com>
2019-08-05 20:51:50 -0400

    Add dynamic sizing preview window

--- src/main/java/plugins/cmds/charts/HistoUtil.java
+++ src/main/java/plugins/cmds/charts/HistoUtil.java
@@ -4,7 +4,15 @@ import java.awt.*;
 
 public class HistoUtil {
 
-    public static final Dimension PREVIEW_DIMENSIONS = new Dimension(300, 200);
+    private static final Dimension screenDims;
+    public static final Dimension PREVIEW_DIMENSIONS;
+
+    static {
+        screenDims = Toolkit.getDefaultToolkit().getScreenSize();
+        PREVIEW_DIMENSIONS = new Dimension((int) screenDims.getWidth() / 2, (int) screenDims.getHeight() / 2);
+    }
+
+
 
     public static final Font HEADER_FONT = new Font(Font.SANS_SERIF, Font.BOLD, 22);
     public static final Font PARAGRAPH_FONT = new Font(Font.SANS_SERIF, Font.PLAIN, 20);
